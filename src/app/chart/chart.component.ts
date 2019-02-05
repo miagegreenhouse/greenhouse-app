06593deb7ff4338f63221374f97ec9e64f3bd673
Jeremy38100 <jeremyroche5@gmail.com>
2019-02-05 09:34:46 +0100

    Min max alerts in chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -12,31 +12,45 @@ export class ChartComponent implements OnInit {
   @Input() title: string;
   @Input() data: ChartData;
   @Input() dateRange = 21600000; // Default value is 6h
+  @Input() min: number = null;
+  @Input() max: number = null;
 
   chart = null;
+  paddingPercent = 0.1;
 
   @ViewChild('chart') canvas;
 
   constructor() {}
 
   ngOnInit() {
+
+    // In order to be more readable add a paddingPercent padding top and bottom in chart
+    const chartBounds: number[] = this.getChartBounds();
+
+    // Add horizontal lines for min and max alert values in chart
+    const annotations = [];
+    if (this.min !== null) {
+      annotations.push(this.getAnnotation(this.min, true));
+    }
+    if (this.max !== null) {
+      annotations.push(this.getAnnotation(this.max, false));
+    }
+
     this.chart = new Chart(this.canvas.nativeElement, {
       type: 'line',
       data: this.data,
       options: {
         scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin: chartBounds[0],
+              suggestedMax: chartBounds[1]
+            }
+          }],
           xAxes: this.getOptionxAxes(this.dateRange)
         },
         annotation: {
-          annotations: [{
-            type: 'line',
-            mode: 'horizontal',
-            scaleID: 'y-axis-0',
-            value: '12',
-            borderColor: 'tomato',
-            borderWidth: 1
-          }],
-          drawTime: "afterDraw" // (default)
+          annotations: annotations
         }
       }
     });
@@ -57,12 +71,65 @@ export class ChartComponent implements OnInit {
     }];
   }
 
+  // Return [min, max]
+  // min: min chart display - paddingPercent % margin
+  // max: maxchart display - paddingPercent % margin
+  private getChartBounds(): number[] {
+    const minMax: number[] = this.getMinMax();
+    const marginChart = (minMax[1] - minMax[0]) * this.paddingPercent;
+
+    minMax[0] = minMax[0] - marginChart;
+    minMax[1] = minMax[1] + marginChart;
+
+    return minMax;
+  }
+
+  // Return [min, max]
+  // min: Min value between all values and min alert value
+  // max: Max value between all values and max alert value
+  private getMinMax(): number[] {
+    if (this.data.datasets.length === 0 && this.data.datasets[0].data.length === 0) {
+      return [0, 0];
+    }
+    const initValue = this.data.datasets[0].data[0].y;
+    const minMax: number[] = [initValue, initValue];
+
+    this.data.datasets.forEach(dataset => {
+      dataset.data.forEach(data => {
+        minMax[0] = Math.min(data.y, minMax[0]);
+        minMax[1] = Math.max(data.y, minMax[1]);
+      });
+    });
+    minMax[0] = Math.min(this.min, minMax[0]);
+    minMax[1] = Math.max(this.max, minMax[1]);
+
+    return minMax;
+  }
+
+  private getAnnotation(value: number, isMin: boolean) {
+    return {
+      type: 'line',
+      mode: 'horizontal',
+      scaleID: 'y-axis-0',
+      value: value,
+      borderColor: '#ff0000',
+      borderWidth: 1,
+      label: {
+        enabled: true,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        fontSize: 12,
+        fontStyle: 'bold',
+        fontColor: '#fff',
+        position: 'left',
+        content: (isMin ? 'min' : 'max') + ' : ' + value
+      }
+    };
+  }
+
 }
 
 export interface ChartData {
-  data: {
-    datasets: Dataset[];
-  };
+  datasets: Dataset[];
 }
 
 export interface Dataset {
