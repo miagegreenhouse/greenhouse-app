da7e0bf3e54b0a9c2b16a8b45d8d01f9aab1d58f
BATISTE <baptiste.rambaud@etu.univ-savoie.fr>
2019-02-11 09:17:54 +0100

    Add error message for threshold

--- src/app/preferences/subComponents/edit-sensor/edit-sensor.component.ts
+++ src/app/preferences/subComponents/edit-sensor/edit-sensor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 import { SensorConfig } from 'src/app/model';
 import {Validators, FormBuilder, FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { minValueValidator, maxValueValidator } from './seuilValidator';
+import { minValueValidator } from './seuilValidator';
 
 
 
@@ -16,57 +16,43 @@ export class EditSensorComponent implements OnInit {
 
   sensorForm: FormGroup;
   sensor:SensorConfig;
+  errorTreshold:boolean = true;
+
   constructor(public navParams: NavParams, public modalController: ModalController, private formBuilder: FormBuilder) {
     this.sensor = this.navParams.get('sensor');
 
-    this.initValidator(this.sensor);
-  }
-
-  initValidator(sensorForm: SensorConfig){
     this.sensorForm = this.formBuilder.group({
-      nomCapteur: [sensorForm.name, Validators.required],
-      unite: [sensorForm.unit, Validators.required],
-      seuilMini: new FormControl(sensorForm.minThresholdValue, Validators.compose([
-        minValueValidator(sensorForm.maxThresholdValue),
-        Validators.required
-      ])),
-      messageSeuilMini: [sensorForm.minThresholdAlertMessage, Validators.required],
-      seuilMaxi: new FormControl(sensorForm.maxThresholdValue, Validators.compose([
-        maxValueValidator(sensorForm.minThresholdValue),
-        Validators.required
-      ])),
-      messageSeuilMaxi: [sensorForm.maxThresholdAlertMessage, Validators.required]
+      nomCapteur: [this.sensor.name, Validators.required],
+      unite: [this.sensor.unit, Validators.required],
+      seuilMini: [this.sensor.minThresholdValue, Validators.required],
+      messageSeuilMini: [this.sensor.minThresholdAlertMessage, Validators.required],
+      seuilMaxi: [this.sensor.maxThresholdValue, Validators.required],
+      messageSeuilMaxi: [this.sensor.maxThresholdAlertMessage, Validators.required]
     });
   }
-  
-
 
   ngOnInit() {
   }
 
   controlThreshold(){
+    let seuilMaxi = Number(this.sensorForm.value['seuilMaxi']);
+    let seuilMini = Number(this.sensorForm.value['seuilMini']);
 
-    let newConfig: SensorConfig;
-    newConfig = {
-      id : '',
-      dataId: '',
-      name : this.sensorForm.value['nomCapteur'],
-      maxThresholdValue : this.sensorForm.value['seuilMaxi'],
-      minThresholdValue : this.sensorForm.value['seuilMini'],
-
-      //TODO remplir ces données lorsque le modèle est à jour
-      unit : this.sensorForm.value['unite'],
-      minThresholdAlertMessage : this.sensorForm.value['messageSeuilMini'],
-      maxThresholdAlertMessage : this.sensorForm.value['messageSeuilMaxi']
-    };
-
-    this.initValidator(newConfig);
+    if(seuilMini > seuilMaxi){
+      this.errorTreshold = false;
+    }
+    else{
+      this.errorTreshold = true;
+    }
   }
 
 
   validateForm(){
-    console.log(this.sensorForm.value);
-    this.modalController.dismiss(); // Enlever NgModel du template HTML
+    this.controlThreshold();
+    if(this.errorTreshold){
+      //TODO : Faire appel au service pour modifier le capteur      
+      this.modalController.dismiss(); // Enlever NgModel du template HTML
+    }
   }
 
   sensorDismiss(){
